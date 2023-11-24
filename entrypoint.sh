a04893b4b895c55b3264ee17227ac92547c7950b
Umair Bussi <engineerhead@gmail.com>
2023-11-24 20:11:28 +0500

    Added commands to build plugins

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,9 @@ source ~/.bashrc
 printf 'Welcome:'
 WORKDIR=/app
 
-cd $WORKDIR/packages/kartifi-plugin-stripe
-npx tsc -b
-cd $WORKDIR/packages/kartifi-plugin-shippo
-npx tsc -b
 cd $WORKDIR
+yarn run build:stripe
+yarn run build:shippo
 yarn install
 
 pm2-runtime start pm2.config.js
