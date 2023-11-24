6f8804f47f6f8ff34c571aae25d6915a85b849aa
Umair Bussi <engineerhead@gmail.com>
2023-11-25 02:03:03 +0500

    Increase start time for health check 1

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,15 @@ WORKDIR=/app
 
 cd $WORKDIR
 yarn install
+if [ "$NODE_ENV" != "development" ];
+then
+  yarn workspaces run build
+fi
 # yarn install
 
 if [ "$NODE_ENV" = "development" ];
 then
   pm2-runtime start pm2.dev.config.js
 else
-  yarn workspaces run build
   pm2-runtime start pm2.config.js
 fi
