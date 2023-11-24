ac83d78267478c24fd47c4470bea4def294f9ff5
Umair Bussi <engineerhead@gmail.com>
2023-11-24 19:45:35 +0500

    Enabled builds of plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,8 @@ WORKDIR=/app
 
 cd $WORKDIR
 yarn install
+cd $WORKDIR/packages/core/kartifi-plugin-stripe
+tsc build
+cd $WORKDIR/packages/core/kartifi-plugin-shippo
+tsc build
 pm2-runtime start pm2.config.js
